2c37ba0af381cfff2b0e72cf35923c772b6b4139
Ighoise O <iodigie03@gmail.com>
2020-06-07 18:09:03 +0000

    Major Update

--- main.cs
+++ main.cs
@@ -11,102 +11,142 @@ using System;
 namespace encryption
 {
   class MainClass {
-
-    public static void decryptPassword(string characters, string passwd, int key){
-      char[] shiftedAlphabet = characters.ToCharArray();
+    public static void decryptPassword(string letters, string specialCharacters, string passwd, int key){
+      char[] shiftedAlphabet = letters.ToCharArray();
+      char[] shiftedCharacters = specialCharacters.ToCharArray();
       for (int a = 0; a < key; a++) {
         //The last letter is saved
         char lastLetter = shiftedAlphabet[shiftedAlphabet.Length - 1];
+        char lastCharacter = shiftedCharacters[shiftedCharacters.Length - 1];
         for (int b = shiftedAlphabet.Length - 1; b > 0; b--) {
           //Every letter up to the last letter is swapped to the 
           shiftedAlphabet[b] = shiftedAlphabet[b - 1];
+          shiftedCharacters[b] = shiftedCharacters[b - 1];
         }
         shiftedAlphabet[0] = lastLetter;
+        shiftedCharacters[0] = lastCharacter;
       }
       string shiftedString = string.Join("", shiftedAlphabet);
+      string characterString = string.Join("", shiftedCharacters);
       char[] passwdArr = passwd.ToCharArray();
       char[] finalPasswdArr = new char[passwd.Length];
+      int placement;
+      char newLetter;
       bool invalid = false;
       for (int i = 0; i < passwd.Length; i++) {
         char passwdLetter = passwdArr[i];
         char upperPasswd = char.ToUpper(passwdLetter);
-        int placement = shiftedString.IndexOf(upperPasswd);
-        if (placement == -1) {
-          invalid = true;
-          break;
+        if(char.IsLetter(upperPasswd)){
+          placement = shiftedString.IndexOf(upperPasswd);
+          newLetter = letters[placement];
+          if (char.IsLower(passwdLetter)) {
+            newLetter = char.ToLower(newLetter);
+          }
         }
-        char newLetter = characters[placement];
-        if (char.IsLower(passwdLetter)) {
-          newLetter = char.ToLower(newLetter);
+        else{
+          placement = characterString.IndexOf(upperPasswd);
+          if (placement == -1) {
+            invalid = true;
+            break;
+          }
+          else {
+            newLetter = specialCharacters[placement];
+          }
         }
         finalPasswdArr[i] = newLetter;
       }
       if (invalid == false) {
         string decryptedPasswd = string.Join("", finalPasswdArr);
-        Console.WriteLine("Decrypted Password: {0}", decryptedPasswd);
-      }
-      else{
-        Console.WriteLine("ENCRYPTED PASSWORD IS INVALID");
-      }
-    }
-
-    public static void encryptPassword(string characters){
-       //The following shifts the each letter in the alpheet by the amount (encryptionKey)
-      float encryptionKey = new Random().Next(1, characters.Length-1);
-      char[] shiftedAlphabet = characters.ToCharArray();
-      for (int a = 0; a < encryptionKey; a++) {
-        //The last letter is saved
-        char lastLetter = shiftedAlphabet[shiftedAlphabet.Length - 1];
-        for (int b = shiftedAlphabet.Length - 1; b > 0; b--) {
-          //Every letter up to the last letter is swapped to the 
-          shiftedAlphabet[b] = shiftedAlphabet[b - 1];
-        }
-        shiftedAlphabet[0] = lastLetter;
-      }
-      Console.WriteLine("Choose an all letter password: ");
-      string passwd = Console.ReadLine();
-      char[] passwdArr = passwd.ToCharArray();
-      char[] finalPasswdArr = new char[passwd.Length];
-      bool invalid = false;
-      for (int i = 0; i < passwd.Length; i++) {
-        char passwdLetter = passwdArr[i];
-        char upperPasswd = char.ToUpper(passwdLetter);
-        int placement = characters.IndexOf(upperPasswd);
-        if (placement == -1) {
-          invalid = true;
-          break;
-        }
-        char newLetter = shiftedAlphabet[placement];
-        if (char.IsLower(passwdLetter)) {
-          newLetter = char.ToLower(newLetter);
-        }
-        finalPasswdArr[i] = newLetter;
-      }
-      if (invalid == false) {
-        string encryptedPasswd = string.Join("", finalPasswdArr);
-        Console.WriteLine("\nEncrypted Password: {0}\n(Your decryption key: {1})\n[Note: Copy the encryption and the Key and try decrypting]", encryptedPasswd, encryptionKey);
+        Console.WriteLine("\nDecrypted Password: {0}", decryptedPasswd);
       }
       else{
         Console.WriteLine("\nINVALID ENTRY\n(PLEASE TRY AGAIN)");
       }
     }
+
+    public static void encryptPassword(string letters, string specialCharacters){
+       //The following shifts the each letter in the alpheet by the amount (encryptionKey)
+      float encryptionKey = new Random().Next(1, letters.Length-1);
+      char[] shiftedAlphabet = letters.ToCharArray();
+      char[] shiftedCharacters = specialCharacters.ToCharArray();
+      for (int a = 0; a < encryptionKey; a++) {
+        //The last letter is saved
+        char lastLetter = shiftedAlphabet[shiftedAlphabet.Length - 1];
+        char lastCharacter = shiftedCharacters[shiftedCharacters.Length - 1];
+        for (int b = shiftedAlphabet.Length - 1; b > 0; b--) {
+          //Every letter up to the last letter is swapped to the 
+          shiftedAlphabet[b] = shiftedAlphabet[b - 1];
+          shiftedCharacters[b] = shiftedCharacters[b - 1];
+        }
+        shiftedAlphabet[0] = lastLetter;
+        shiftedCharacters[0] = lastCharacter;
+      }
+      Console.WriteLine("\nChoose a valid password: ");
+      string passwd = Console.ReadLine();
+      char[] passwdArr = passwd.ToCharArray();
+      char[] finalPasswdArr = new char[passwd.Length];
+      int placement;
+      char newLetter;
+      bool invalid = false;
+      for (int i = 0; i < passwd.Length; i++) {
+        char passwdLetter = passwdArr[i];
+        char upperPasswd = char.ToUpper(passwdLetter);
+        if (char.IsLetter(upperPasswd)) {
+          placement = letters.IndexOf(upperPasswd);
+          newLetter = shiftedAlphabet[placement];
+          if (char.IsLower(passwdLetter)) {
+            newLetter = char.ToLower(newLetter);
+          }
+        }
+        else{
+          placement = specialCharacters.IndexOf(upperPasswd);
+          if (placement == -1) {
+            invalid = true;
+            break;
+          }
+          else {
+            newLetter = shiftedCharacters[placement];
+          }
+        }
+        finalPasswdArr[i] = newLetter;
+      }
+      if (invalid == false) {
+        string encryptedPasswd = string.Join("", finalPasswdArr);
+        Console.WriteLine("\nEncrypted Password: {0}\n(Your decryption key: {1})\n\n\n[Note: Copy the encrypted password along with the key and try decrypting]", encryptedPasswd, encryptionKey);
+      }
+      else{
+        Console.WriteLine("\nINVALID ENTRY\n(PLEASE TRY AGAIN)");
+      }
+    }
+    
     public static void Main (string[] args) {
+      //This is the title
+      Console.ForegroundColor = ConsoleColor.Red;
+      Console.WriteLine("Ywaown+Yeldan: Encryption Service\n(Key = 4)");
+      Console.ResetColor();
 
       //In the following variable I initialize all valid characters for the user password
-      string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//_!@#$%^&*()?1234567890";
+      string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string specialCharacters = "1234567890!@#$%^&*-_.+|:? ";
       
       //First the user is asked what they want to do
-      Console.WriteLine("Do you have a previous password to decrypt (Y or N): ");
+      Console.WriteLine("\nDo you have a previous password to decrypt (Y or N): ");
       string isDecrypt = Console.ReadLine();
+      isDecrypt = isDecrypt.ToUpper();
       if (string.Equals(isDecrypt, "Y")) {
-        Console.WriteLine("What is your key: ");
-        int key = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("What was the encrypted password: ");
-        string passwd = Console.ReadLine();
-        decryptPassword(characters, passwd, key);
+        Console.WriteLine("\nWhat is your key: ");
+        string userInput = Console.ReadLine();
+        if (int.TryParse(userInput, out int key)){
+          Console.WriteLine("\nWhat was the encrypted password: ");
+          string passwd = Console.ReadLine();
+          decryptPassword(letters, specialCharacters, passwd, key);
+        }
+        else{
+          Console.WriteLine("\nINVALID ENTRY\n(PLEASE TRY AGAIN)");
+        }
       }
       else if (string.Equals(isDecrypt, "N")){
-        encryptPassword(characters);
+        encryptPassword(letters, specialCharacters);
       }
       else {
         Console.WriteLine("\nINVALID ENTRY\n(PLEASE TRY AGAIN)");
